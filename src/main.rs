23e11d3982a3398dc86a63c9c481cd39536acee6
falkwitte <krixcrox@duck.com>
2024-07-18 18:55:53 +0200

    basic stuff done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{fs::read_to_string, ptr::read};
+use cryptoys;
+
+#[derive(Parser)]
+pub struct Args{
+    pub filename: std::path::PathBuf,
+    pub key: String,
+    
+}
+
+fn main(){
+    let args = Args::parse();
+
+    if !args.filename.to_str().unwrap().is_empty() {
+        // convert a string to a path
+       let stuff = read_to_string(args.filename).unwrap(); 
+       let encrypt = cryptoys::historical::playfair::encrypt(&stuff, &args.key);
+
+       println!("{encrypt}"); 
+    }
 }
